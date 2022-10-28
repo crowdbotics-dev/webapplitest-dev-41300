c9f45b6c2df101423ec881046ec6c6f64bbf52a9
Crowdbotics <engineering@crowdbotics.com>
2022-10-28 03:15:36 +0000

    Initial commit.

---
+++ webapplitest_dev_41300/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for webapplitest_dev_41300 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapplitest_dev_41300.settings')
+
+application = get_wsgi_application()
